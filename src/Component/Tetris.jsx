0d4d750d1a1ca922605b7b9ddaeee7468ba3d7d6
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-16 14:28:42 -0400

    Tetris comp wraped with style comp

--- src/Component/Tetris.jsx
+++ src/Component/Tetris.jsx
@@ -5,17 +5,19 @@ import Display from "./Display";
 import StartButton from "./StartButton";
 function Tetris() {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 }
 
